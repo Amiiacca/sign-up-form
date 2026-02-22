4a954b3e646b88e99552b220e71a642940c1d44f
Amiiacca <santaamaitsu@gmail.com>
2026-02-22 01:30:07 -0600

    User fields implementation: first name, last name, age, phone number, email, password

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useMemo, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable, KeyboardAvoidingView, Platform} from 'react-native';
+import { RadioGroup } from 'react-native-radio-buttons-group';
 
 export default function App() {
+
+
+  const radioButtons = useMemo(() => ([
+    {
+      id: '1', label: 'Male', value: 'male'
+    },
+
+    {
+      id: '2', label: 'Female', value: 'female'
+    }
+  ]), []);
+
+  const [selectedId, setSelectedId] = useState()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <KeyboardAvoidingView behavior={Platform.OS === 'android' ? "padding" : undefined}>
+
+      <Text>Sign-Up form</Text>
+      <TextInput style={styles.firstName} placeholder='First Name'/>
+      <TextInput style={styles.lastName} placeholder='Last Name'/>
+        {/* Here should go radio button */}
+      <RadioGroup
+        radioButtons={radioButtons}
+        onPress={setSelectedId}
+        selectedId={selectedId}
+      />
+
+      <TextInput style={styles.agePicker} placeholder='Age'></TextInput>
+      <TextInput style={styles.phoneNumber} placeholder='Phone Number'></TextInput>
+      <TextInput style={styles.userEmail} placeholder='Email'></TextInput>
+      <TextInput style={styles.password} placeholder='Password'></TextInput>
+
+      <Button title='Submit'></Button>
+
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -17,4 +51,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
